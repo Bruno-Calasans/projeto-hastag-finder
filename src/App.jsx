94315a6906012f55c364cdaf34a2b982ab3fd9ae
Bruno Calasans <83395524+Bruno-Calasans@users.noreply.github.com>
2022-08-18 15:27:31 -0300

    Criação das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 
+import { Routes,  Route} from 'react-router-dom'
+import { GlobalStyle } from './App.style.js'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import List from './pages/List'
+import About from './pages/About'
+
 function App() {
-  return <div>Teste</div> 
+
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/list' element={<List/>}/>
+        <Route path='/' element={<About />}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ src/pages/List/index.jsx
@@ -0,0 +1,4 @@
+export default function List() {
+
+    return <div>Página List</div>
+}
