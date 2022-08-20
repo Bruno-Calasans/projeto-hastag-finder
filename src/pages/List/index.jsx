3a31b02ead75dbc33258d73d840ac637b0e7d565
maeduarda <mess22@discente.ifpe.edu.br>
2022-08-20 12:03:48 -0300

    criação da lista de buscas

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,8 +1,67 @@
+
+import { Layout } from '../../Layout'
 export default function List() {
+
+    const Itens = [
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        {
+            hashtag: '#hashtag',
+            data: '25/05', 
+            hour: '09:30'
+        },
+        
+    ]
+
 
     return (
     <Layout>
-    <div>Página List</div>   
+        <div>
+            <h3>Buscas Realizadas</h3>
+            <div>
+                <h3>Hastag</h3>
+                <h3>Data</h3>
+                <h3>Hora</h3>
+            </div>
+            <div>
+               {Itens.map((item)=> (
+                <div>
+                    <p>{item.hashtag}</p>
+                    <p>{item.data}</p>
+                    <p>{item.hour}</p>
+                </div>
+               ))}
+            </div>
+
+        </div>
     </Layout>
     )
 }
